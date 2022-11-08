6e85f9f58e1a68e447385f0bca71c121520f19d8
JengPH <110097610+JengPH@users.noreply.github.com>
2022-11-08 23:34:13 +0800

    Add files via upload

---
+++ Bartendance/Bartendance/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿
+namespace Bartendance
+{
+    partial class singupform
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelname = new System.Windows.Forms.Label();
+            this.txtname = new System.Windows.Forms.TextBox();
+            this.txtemail = new System.Windows.Forms.TextBox();
+            this.txtpass = new System.Windows.Forms.TextBox();
+            this.txtcpass = new System.Windows.Forms.TextBox();
+            this.Email = new System.Windows.Forms.Label();
+            this.password = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.signup = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelname
+            // 
+            this.labelname.AutoSize = true;
+            this.labelname.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelname.Location = new System.Drawing.Point(156, 52);
+            this.labelname.Name = "labelname";
+            this.labelname.Size = new System.Drawing.Size(96, 29);
+            this.labelname.TabIndex = 0;
+            this.labelname.Text = "Name :";
+            // 
+            // txtname
+            // 
+            this.txtname.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtname.Location = new System.Drawing.Point(279, 47);
+            this.txtname.Name = "txtname";
+            this.txtname.Size = new System.Drawing.Size(264, 34);
+            this.txtname.TabIndex = 1;
+            // 
+            // txtemail
+            // 
+            this.txtemail.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtemail.Location = new System.Drawing.Point(279, 128);
+            this.txtemail.Name = "txtemail";
+            this.txtemail.Size = new System.Drawing.Size(264, 34);
+            this.txtemail.TabIndex = 2;
+            // 
+            // txtpass
+            // 
+            this.txtpass.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtpass.Location = new System.Drawing.Point(279, 217);
+            this.txtpass.Name = "txtpass";
+            this.txtpass.PasswordChar = '*';
+            this.txtpass.Size = new System.Drawing.Size(264, 34);
+            this.txtpass.TabIndex = 3;
+            // 
+            // txtcpass
+            // 
+            this.txtcpass.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtcpass.Location = new System.Drawing.Point(279, 304);
+            this.txtcpass.Name = "txtcpass";
+            this.txtcpass.PasswordChar = '*';
+            this.txtcpass.Size = new System.Drawing.Size(264, 34);
+            this.txtcpass.TabIndex = 4;
+            // 
+            // Email
+            // 
+            this.Email.AutoSize = true;
+            this.Email.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Email.Location = new System.Drawing.Point(159, 133);
+            this.Email.Name = "Email";
+            this.Email.Size = new System.Drawing.Size(93, 29);
+            this.Email.TabIndex = 5;
+            this.Email.Text = "Email :";
+            // 
+            // password
+            // 
+            this.password.AutoSize = true;
+            this.password.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.password.Location = new System.Drawing.Point(110, 222);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(142, 29);
+            this.password.TabIndex = 6;
+            this.password.Text = "Password :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 304);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(240, 29);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Confirm Password :";
+            // 
+            // signup
+            // 
+            this.signup.Location = new System.Drawing.Point(444, 366);
+            this.signup.Name = "signup";
+            this.signup.Size = new System.Drawing.Size(99, 34);
+            this.signup.TabIndex = 8;
+            this.signup.Text = "Sign Up";
+            this.signup.UseVisualStyleBackColor = true;
+            this.signup.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // singupform
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(682, 483);
+            this.Controls.Add(this.signup);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.Email);
+            this.Controls.Add(this.txtcpass);
+            this.Controls.Add(this.txtpass);
+            this.Controls.Add(this.txtemail);
+            this.Controls.Add(this.txtname);
+            this.Controls.Add(this.labelname);
+            this.Name = "singupform";
+            this.Text = "Sign Up";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelname;
+        private System.Windows.Forms.TextBox txtname;
+        private System.Windows.Forms.TextBox txtemail;
+        private System.Windows.Forms.TextBox txtpass;
+        private System.Windows.Forms.TextBox txtcpass;
+        private System.Windows.Forms.Label Email;
+        private System.Windows.Forms.Label password;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button signup;
+    }
+}
+
---
+++ Bartendance/Bartendance/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bartendance
+{
+    public partial class singupform : Form
+    {
+        public singupform()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\EJ\Documents\signup.mdf;Integrated Security=True;Connect Timeout=30");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("insert into detail values('"+txtname.Text+ "','" +txtemail.Text+ "','" + txtpass.Text + "', '" + txtcpass.Text + "')", con);
+
+            cmd.ExecuteNonQuery();
+
+            MessageBox.Show("Sign Up Successfully");
+
+            txtname.Clear();
+            txtemail.Clear();
+            txtpass.Clear();
+            txtcpass.Clear();
+            
+        }
+    }
+}
